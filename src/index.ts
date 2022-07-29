f2cf1d1f716a2adb620df4bef6dff392be830ba7
Ajetunmobi Isaac <isaacajetunmobi@gmail.com>
2022-07-29 21:53:23 +0100

    init

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const app = express();
-const port = 5000;
-app.get('/home', (_, res) => {
-  res.status(200).send('I am here');
-});
-app.listen(port, () => console.log(`Running on port ${port}`));
